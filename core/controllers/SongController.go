e876eb35013cb43a05d1369f57ca347b321e8266
nasuhyc <nasuhyc@gmail.com>
2022-11-21 22:26:57 +0300

    gorm-postgres relationships

--- core/controllers/SongController.go
+++ core/controllers/SongController.go
@@ -3,9 +3,12 @@ package controllers
 import (
 	"golang-gorm-relationships/config"
 	"golang-gorm-relationships/models"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
+
+//DESC - Get all songs and their artists
 
 func SongIndex(c *fiber.Ctx) error {
 	var songs []models.Song
@@ -17,32 +20,69 @@ func SongIndex(c *fiber.Ctx) error {
 
 }
 
+//DESC - Create a new song and add artists to it
 func SongStore(c *fiber.Ctx) error {
-	song := models.Song{}
-	artis := models.Artist{}
-	if err := c.BodyParser(&song); err != nil {
-		return c.Status(400).JSON(err.Error())
+
+	type SongData struct {
+		ID       uint            `gorm:"primaryKey"`
+		Title    string          `json:"title"`
+		SongTime float64         `json:"song_time"`
+		Artists  []models.Artist `json:"artists"`
 	}
-	if err := c.BodyParser(&artis); err != nil {
+
+	songData := SongData{}
+
+	if err := c.BodyParser(&songData); err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
 
-	song = models.Song{
-		Title:    song.Title,
-		SongTime: song.SongTime,
+	song := models.Song{
+		Title:    songData.Title,
+		SongTime: songData.SongTime,
+		ID:       songData.ID,
+		Artists:  songData.Artists,
 	}
-	config.DB.Create(&song)
-	config.DB.Model(&song).Association("Artists").Append(&artis)
-	return c.Status(200).JSON(fiber.Map{"data": artis, "message": "Create artist successfully."})
+	config.DB.Omit("Artist").Save(&song)
+
+	return c.Status(200).JSON(fiber.Map{"data": song, "message": "Create song successfully."})
 }
+
+//DESC - Update a song and replace artists to it
 func SongUpdate(c *fiber.Ctx) error {
-	return c.SendString("sa")
+	id := c.Params("id")
+	U, err := strconv.ParseUint(id, 0, 64)
+	if err != nil {
+		return err
+	}
+	newid := uint(U)
+	var client_query *models.Song
+	config.DB.Where("id = ?", id).Preload("Artists").First(&client_query)
+	config.DB.Model(&models.Song{ID: newid}).Association("Artists").Replace(&client_query.Artists)
+	return c.Status(200).JSON(fiber.Map{"data": &client_query, "message": "Updated is Successfully."})
 }
+
+//DESC - Bringing songs and artists according to ID
 
 func SongGetId(c *fiber.Ctx) error {
-	return c.SendString("sa")
+	id := c.Params("id")
+	var songs []models.Song
+	err := config.DB.Model(&models.Artist{}).Preload("Songs").Find(&songs, id).Error
+	if err != nil {
+		return c.Status(400).JSON(err)
+	}
+	return c.Status(200).JSON(songs)
 }
 
+//DESC - Delete a song and delete artists to it
 func SongDestroy(c *fiber.Ctx) error {
-	return c.SendString("sa")
+	id := c.Params("id")
+	U, err := strconv.ParseUint(id, 0, 64)
+	if err != nil {
+		return err
+	}
+	newid := uint(U)
+	var client_query *models.Song
+	config.DB.Where("id = ?", id).Preload("Artists").First(&client_query).Delete(client_query)
+	config.DB.Model(&models.Song{ID: newid}).Association("Artists").Delete(&client_query.Artists)
+	return c.Status(200).JSON(fiber.Map{"data": &client_query, "message": "Deleted is Successfully."})
 }
--- core/models/ArtistModel.go
+++ core/models/ArtistModel.go
@@ -1,8 +1,17 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Artist struct {
-	ID      float64 `gorm:"primaryKey"`
-	Name    string  `json:"name"`
-	Surname string  `json:"surname" `
-	Songs   []Song  `gorm:"many2many:artist_at_song" json:"songs" `
+	ID        uint           `gorm:"primaryKey"`
+	Name      string         `json:"name"`
+	Surname   string         `json:"surname" `
+	Songs     []Song         `gorm:"many2many:artist_at_song" json:"songs" `
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
